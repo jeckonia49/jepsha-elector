a2c0ae0d92fd755f0455dc1131326260b0e87de6
django unleash <ckwriter72@gmail.com>
2023-10-28 15:16:03 +0300

    modfiy administrator access

--- e_voting/settings.py
+++ e_voting/settings.py
@@ -15,9 +15,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "tufwegkhferjiyfgdvsjcgwfsiycjhvvcdsyufevifgewauyfdsuyfcew"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["jepsha.pythonanywhere.com"]
 
 
 # Application definition
