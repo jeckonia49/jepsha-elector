399a99f23bb530a5cf65d2ba7e1892429b86902a
django unleash <ckwriter72@gmail.com>
2023-11-19 10:07:37 +0300

    trailing slash

--- administrator/urls.py
+++ administrator/urls.py
@@ -5,24 +5,24 @@ from . import views
 urlpatterns = [
     path("", views.dashboard, name="adminDashboard"),
     # * Voters
-    path("voters", views.voters, name="adminViewVoters"),
-    path("voters/view", views.view_voter_by_id, name="viewVoter"),
-    path("voters/delete", views.DeleteVoter.as_view(), name="deleteVoter"),
+    path("voters/", views.voters, name="adminViewVoters"),
+    path("voters/view/", views.view_voter_by_id, name="viewVoter"),
+    path("voters/delete/", views.DeleteVoter.as_view(), name="deleteVoter"),
     # path("voters/delete", views.deleteVoter, name="deleteVoter"),
     # path("voters/update", views.updateVoter, name="updateVoter"),
     # # * Position
-    path("position/view", views.view_position_by_id, name="viewPosition"),
-    path("position/update", views.updatePosition, name="updatePosition"),
-    path("position/delete", views.DeletePosition.as_view(), name="deletePosition"),
-    path("positions/view", views.viewPositions, name="viewPositions"),
+    path("position/view/", views.view_position_by_id, name="viewPosition"),
+    path("position/update/", views.updatePosition, name="updatePosition"),
+    path("position/delete/", views.DeletePosition.as_view(), name="deletePosition"),
+    path("positions/view/", views.viewPositions, name="viewPositions"),
     # # * Candidate
     path("candidate/", views.viewCandidates, name="viewCandidates"),
-    path("candidate/update", views.updateCandidate, name="updateCandidate"),
-    path("candidate/delete", views.DeleteCandidate.as_view(), name="deleteCandidate"),
-    path("candidate/view", views.view_candidate_by_id, name="viewCandidate"),
+    path("candidate/update/", views.updateCandidate, name="updateCandidate"),
+    path("candidate/delete/", views.DeleteCandidate.as_view(), name="deleteCandidate"),
+    path("candidate/view/", views.view_candidate_by_id, name="viewCandidate"),
     # # * Settings (Ballot Position and Election Title)
     path(
-        "settings/ballot/position",
+        "settings/ballot/position/",
         views.BallotPositionView.as_view(),
         name="ballot_position",
     ),
@@ -33,12 +33,12 @@ urlpatterns = [
         name="update_ballot_position",
     ),
     # # * Votes
-    path("votes", views.VotesListView.as_view(), name="viewVotes"),
-    path("votes/view", views.view_vote_by_id, name="viewVote"),
+    path("votes/", views.VotesListView.as_view(), name="viewVotes"),
+    path("votes/view/", views.view_vote_by_id, name="viewVote"),
     path("votes/reset/", views.ResetVotesView.as_view(), name="resetVote"),
     path("votes/results/", views.PrintView.as_view(), name="printResult"),
-    path("votes/results/view", views.DownloadElectionResults.as_view(), name="result"),
-    path("votes/delete", views.DeleteVote.as_view(), name="deleteVote"),
+    path("votes/results/view/", views.DownloadElectionResults.as_view(), name="result"),
+    path("votes/delete/", views.DeleteVote.as_view(), name="deleteVote"),
     # # * Messages
     path("mailbox/", views.ElectionMilboxView.as_view(), name="viewMessages"),
     path(
